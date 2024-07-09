f6547999b8407248fbf385e3ca2fe8df04cba851
Sivan <Sivan@SIVAN-DESKTOP>
2024-07-09 09:43:14 -0500

    Add project files.

---
+++ Example.Services/Services/IExampleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Services.Services
+{
+        public interface IExampleService
+        {
+            string GetGreeting(string name);
+        }
+    
+}
---
+++ Example.Services/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Example.Services.Services
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddExampleServices(this IServiceCollection services)
+        {
+            services.AddTransient<IExampleService, ExampleService>();
+            return services;
+        }
+    }
+}
